277c2b8f7db044ea1e480fb44f0dca78e9bd398a
Joseph Miles <josephmiles2015@gmail.com>
2019-05-18 21:40:23 -0700

    Implemented crude damage computation.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-import csv, argparse, sys, math, re
+import csv, argparse, sys, math, re, fractions
 import yaml
 
-DATA_DIR = '../test_data/'
+DATA_DIR = './data/'
 
 def csv_to_dict(filepath):
     '''
@@ -9,7 +9,7 @@ def csv_to_dict(filepath):
     into a list of dictionaries. Each entry in the returned dictionaries has a
     key corresponding to the values in the first row.
 
-    @NOTE! This assumes that the first row in the CSV are the column names!
+    NOTE[joe] This assumes that the first row in the CSV are the column names!
     '''
 
     data = list()
@@ -38,8 +38,8 @@ def csv_to_dict(filepath):
 def parse_monster_balance_table(csv_data):
     '''
     This function takes in a list of dictionaries (retreived using csv_to_dict)
-    and converts it into a data structure that encodes the monster balance table
-    that is found in the Dungeon Master's Guide on page 274.
+    and converts it into a data structure that encodes the monster balance
+    table that is found in the Dungeon Master's Guide on page 274.
     '''
 
     def parse_range(string):
@@ -65,25 +65,27 @@ def compute_cr(monster_data, balance_table):
     '''
 
     def average_roll(expression):
-        values = re.match(r'(\d+)d(\d+)([+-]\d+)', expression)
+        values = re.match(r'(\d+)d(\d+)([+-]\d+)', expression).groups()
         values = [ int(i) if i else 0 for i in values ]
 
-        minimum = values[0]
-        maximum = values[0]*values[1] + values[2]
+        minimum = values[0] + values[2]
+        maximum = (values[0] * values[1]) + values[2]
 
         # This will round down instead of up
         return int((minimum + maximum) / 2)
+
+    def cr_to_num(cr):
+        cr_value = fractions.Fraction(cr)
+        return cr_value.numerator / cr_value.denominator
 
     # Compute Defensive CR.
     defensive_cr = 0
 
     #   Compute CR from HP.
+    avg = average_roll(monster_data['hitpoints'])
     for entry in balance_table:
-
-        avg = average_roll(mosnter_data['hp'])
-
         if avg >= entry['hitpoints'][0] and avg <= entry['hitpoints'][1]:
-            defensive_cr = entry['cr']
+            defensive_cr = cr_to_num(entry['cr'])
             break
 
     #   Compute CR Offset from AC.
@@ -91,7 +93,7 @@ def compute_cr(monster_data, balance_table):
 
     for entry in balance_table:
         if monster_data['armor']['class'] == entry['armor_class']:
-            cr_from_ac = entry['cr']
+            cr_from_ac = cr_to_num(entry['cr'])
             break
 
     # Another place where we're rounding down. I don't know if that's a good
@@ -103,38 +105,55 @@ def compute_cr(monster_data, balance_table):
 
     #   Compute CR from Average Damage.
     '''
-    After a long time of looking at how to compute average damage, I've come
-    to the conclusion that this is a monsterously (heh) complicated problem
-    that will take a very long time to formulate a ready solution for.
+    FIXME[joe] This is a really bad approximation for damage output of a
+    monster. This is totalling how much damage all their attack's do and
+    dividing by the number of attacks.
     '''
+    avg_dpr = 0
+    damaging_actions = 0
+
+    for action in monster_data['actions']:
+        if action['damage']:
+            avg_dpr = max(avg_dpr, average_roll(action['damage']))
+            damaging_actions += 1
+
+    avg_dpr = avg_dpr / damaging_actions
+
     for entry in balance_table:
+        avg_dmg = entry['average_damage']
 
-        avg_dpr = 0
-
-        for action in monster_data['actions']:
-            avg_dpr = avg_dpr + average_roll(action['damage'])
-
-        if avg >= entry['hitpoints'][0] and avg <= entry['hitpoints'][1]:
-            defensive_cr = entry['cr']
+        if avg_dpr >= avg_dmg[0] and avg_dpr <= avg_dmg[1]:
+            offensive_cr = cr_to_num(entry['cr'])
             break
 
     #   Compute CR Offset from Attack Bonus (or save DC).
+    attack_bonuses = [ action['to_hit'] for action in monster_data['actions'] ]
+
+    cr_from_attack_bonus = 0
+    for entry in balance_table:
+        if max(attack_bonuses) == entry['attack_bonus']:
+            cr_from_attack_bonus = cr_to_num(entry['cr'])
+            break
+
+    offensive_cr = offensive_cr - int((offensive_cr - cr_from_attack_bonus) / 2)
+
+    print("Offensive CR: "+str(offensive_cr))
+    print("Defensive CR: "+str(defensive_cr))
 
     # Compute Average CR. (Round to nearest CR)
+    return int((offensive_cr + defensive_cr) / 2)
 
-def main():
-    global DATA_DIR
 
-    mosnter_block = None
+if __name__ == '__main__':
+    monster_block = None
 
     with open(DATA_DIR+'gargoyle.yaml') as f:
-        monster_block = yaml.load(f)
+        monster_block = yaml.full_load(f)
 
-    # print(monster_block)
+    print(monster_block)
 
     csv_data = csv_to_dict(DATA_DIR+'monster_balancing_table.csv')
 
     monster_balance_table = parse_monster_balance_table(csv_data)
 
-if __name__ == '__main__':
-    main()
+    print(compute_cr(monster_block, monster_balance_table))
